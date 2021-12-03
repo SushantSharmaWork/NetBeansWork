1fcb4aa067bad767cc5e76c757b2748e7c5ed000
shubi <shubi@DESKTOP-9T78VHM>
2021-12-03 23:54:54 +0530

    Merge origin/main

---
+++ src/JDBCandDatabase/Insert.java
@@ -0,0 +1,29 @@
+
+package JDBCandDatabase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author shubi
+ */
+public class Insert  {
+     public static void main(String[] args) throws Exception {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cu","root","root");
+        String s ="insert into student values(?,?,?,?,?)"; //no. of columns
+        PreparedStatement p = con.prepareStatement(s);
+        p.setString(1,"1");
+        p.setString(2,"Sushant");
+        p.setString(3,"php");
+        p.setString(4,"2@gmail.com");
+        p.setString(5,"kol");
+        int i = p.executeUpdate();
+        if(i>0){
+            System.out.println("Done");
+        }
+     }
+    
+}
